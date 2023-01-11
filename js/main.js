f610aa2e6fa507642adc4d71253ffd4be765854a
JHay0112 <jordanhay0112@gmail.com>
2023-01-11 13:53:42 +1300

    begin including js backend

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/**
+ * Run upon load of lgui
+ * Loads in canvas and 
+ */
+
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+/**
+ * Draws the grid of the 
+ */
+function drawGrid() {
+
+}
