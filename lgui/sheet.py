82110df9213a4e9e9ed6079e8e0ede7d593aeff7
JHay0112 <jordanhay0112@gmail.com>
2023-01-12 11:06:20 +1300

    layout sheet

--- lgui/sheet.py
+++ lgui/sheet.py
@@ -1,3 +1,25 @@
 """
 Defines a grid sheet for laying out lgui components on.
 """
+
+from components import Component
+
+class Sheet:
+
+    """
+    Schematic sheet.
+
+    Parameters
+    ----------
+
+    name: str
+        The name of the sheet.
+    author: str
+        The author who produced the sheet.
+    """
+
+    def __init__(self, name: str, author: str):
+
+        self.name: str = name
+        self.author: str = author
+        self.components: list[Component]
