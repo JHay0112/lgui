da08b99b12d7e9e78294f4596c735709fe104235
JHay0112 <jordanhay0112@gmail.com>
2023-01-12 10:54:45 +1300

    begin potential structure of component class

--- lgui/components.py
+++ lgui/components.py
@@ -1,3 +1,51 @@
 """
 Defines the components that lgui can simulate
 """
+
+class Component:
+
+    """
+    Describes an lgui component.
+
+    Parameters
+    ----------
+
+    ctype: str
+        The type of the component selected from Component.TYPES
+    """
+
+    ORIENTATIONS = ("N", "E", "S", "W")
+    """Component orientations"""
+    N = ORIENTATIONS[0]
+    E = ORIENTATIONS[1]
+    S = ORIENTATIONS[2]
+    W = ORIENTATIONS[3]
+
+    TYPES = ("R", "L", "C")
+    """Component types"""
+    R = TYPES[0]
+    L = TYPES[1]
+    C = TYPES[2]
+
+    GRID_HEIGHT = 5
+    """Height of components on the editor grid"""
+
+    next_ids = {ctype: 0 for ctype in TYPES}
+
+    def __init__(self, ctype: str, value: int | float | str):
+
+        self.type = ctype
+        self.value = value
+        self.orientation = Component.N
+        self.pos_x = 0
+        self.pos_y = 0
+        self.id = Component.next_ids[self.type]
+        Component.next_ids[self.type] += 1
+
+    def rotate(self):
+        """
+        Rotates the component by 90 degrees clockwise.
+        """
+        self.orientation += Component.ORIENTATIONS[
+            (Component.ORIENTATIONS.index(self.orientation) + 1) & len(Component.ORIENTATIONS)
+        ] # go to next orientation in the orientation list
