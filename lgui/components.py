0be43029a5f7926f482f355ed8c0ca62c67fd232
JHay0112 <jordanhay0112@gmail.com>
2023-01-18 09:54:34 +1300

    draw inductors

--- lgui/components.py
+++ lgui/components.py
@@ -112,11 +112,39 @@ class Component:
                 case Component.R: # Resistors
                     ...
                 case Component.L: # Inductors
-                    ...
+
+                    LOOPS = 4
+                    LEAD_LENGTH = 0.2
+                    LOOP_RADIUS = (1 - 2*LEAD_LENGTH)/(2*LOOPS)
+
+                    # leads
+                    offset = self.along(LEAD_LENGTH)
+                    mid = offset + (start_x, start_y)
+                    layer.stroke_line(start_x, start_y, mid[0], mid[1])
+                    mid = (end_x, end_y) - offset
+                    layer.stroke_line(mid[0], mid[1], end_x, end_y)
+
+                    angle_offset = np.arccos(
+                        np.clip(
+                            np.dot(
+                                offset/np.linalg.norm(offset), np.array([1, 0])
+                            ), 
+                        -1.0, 1.0)
+                    ) % np.pi
+
+                    # loops
+                    for l in range(LOOPS):
+                        mid = self.along(LEAD_LENGTH + (2*l + 1)*LOOP_RADIUS) + (start_x, start_y)
+                        layer.stroke_arc(
+                            mid[0], mid[1], 
+                            LOOP_RADIUS*editor.STEP/editor.SCALE, 
+                            np.pi - angle_offset, -angle_offset 
+                        )
+                    
                 case Component.C: # Capacitors
 
                     PLATE_WIDTH = 0.4
-                    PLATE_SEP = 0.025
+                    PLATE_SEP = 0.03
 
                     # lead 1
                     mid = self.along(0.5 - PLATE_SEP) + (start_x, start_y)
