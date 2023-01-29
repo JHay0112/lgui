db49f40d56f0ece53c21303e020045d6311fc35a
Michael Hayes <michael.hayes@canterbury.ac.nz>
2023-01-30 08:24:42 +1300

    Use 300 dpi

--- lgui/ui/matplotlib_tkinter/expr_dialog.py
+++ lgui/ui/matplotlib_tkinter/expr_dialog.py
@@ -99,7 +99,8 @@ class ExprDialog:
         # Need amsmath for operatorname
         template = ('\\documentclass[a4paper]{standalone}\n'
                     '\\usepackage{amsmath}\n'
-                    '\\begin{document}\n$%s$\\end{document}')
+                    '\\begin{document}\n$%s$\n'
+                    '\\end{document}\n')
         content = template % e.latex()
 
         open(tex_filename, 'w').write(content)
@@ -109,7 +110,7 @@ class ExprDialog:
 
         png_filename = tex_filename.replace('.tex', '.png')
         pdfconverter = PDFConverter()
-        pdfconverter.to_png(pdf_filename, png_filename, dpi=600)
+        pdfconverter.to_png(pdf_filename, png_filename, dpi=300)
 
         img = ImageTk.PhotoImage(Image.open(png_filename), master=self.master)
         return img
